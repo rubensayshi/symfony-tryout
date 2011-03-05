76317f657fd02f6b50cf4ffec06de63ba8b546d2
root <root@lamp.(none)>
2011-03-05 21:07:18 +0000

    Playing around with symfony bundles

--- src/Sensio/HelloBundle/Controller/HelloController.php
+++ src/Sensio/HelloBundle/Controller/HelloController.php
@@ -8,14 +8,11 @@ class HelloController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('HelloBundle:Hello:index.html.twig', array('name' => $name));
-
-        // render a PHP template instead
-        // return $this->render('HelloBundle:Hello:index.html.php', array('name' => $name));
+        return $this->render('HelloBundle:Hello:index.html.php', array('name' => $name));
     }
 
     public function welcomeAction()
     {
-        return $this->render('HelloBundle:Hello:welcome.html.twig');
+        return $this->render('HelloBundle:Hello:welcome.html.php');
     }
 }
